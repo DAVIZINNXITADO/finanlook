04be7734c8747c333feca1593564eae80aacf7df
DAVIZINNXITADO <davigoncalvesnoronhapbu02@gmail.com>
2026-09-04 09:05:32 -0300

    Atualizar o client.server.ts

--- src/integrations/supabase/client.server.ts
+++ src/integrations/supabase/client.server.ts
@@ -1,13 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 import type { Database } from "./types";
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_PUBLISHABLE_KEY =
-  import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_PUBLISHABLE_KEY = process.env.SUPABASE_PUBLISHABLE_KEY;
 
 if (!SUPABASE_URL || !SUPABASE_PUBLISHABLE_KEY) {
   throw new Error(
-    "Missing VITE_SUPABASE_URL or VITE_SUPABASE_PUBLISHABLE_KEY",
+    "Missing SUPABASE_URL or SUPABASE_PUBLISHABLE_KEY",
   );
 }
 
@@ -16,10 +15,8 @@ export const supabase = createClient<Database>(
   SUPABASE_PUBLISHABLE_KEY,
   {
     auth: {
-      persistSession: true,
-      autoRefreshToken: true,
-      detectSessionInUrl: true,
-      flowType: "pkce",
+      persistSession: false,
+      autoRefreshToken: false,
     },
   },
 );
